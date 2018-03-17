940697b4d5327d57e0c8d4aeada59cf091bc3780
David Carlson <37191405+Figureix@users.noreply.github.com>
2018-03-17 17:07:32 -0600

    Hello World!

---
+++ Makefile
@@ -0,0 +1,4 @@
+assn5: assn5.c Makefile
+	gcc -g -Wextra -Wall -pedantic -o assn5 assn5.c
+
+
---
+++ assn5.c
@@ -0,0 +1,17 @@
+/*
+Collaborative Extra Credit Effort
+David C
+Jason S
+ECE 3060-601
+Assignment 5
+
+Originality Disclaimer: The following code we have written under our own efforts -- we have not used anothers work(s) to pass as ours
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	printf("Hello World!\n");
+	return 0;
+}
+
